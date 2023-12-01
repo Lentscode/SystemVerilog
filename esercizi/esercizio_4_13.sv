0129eed1471deab110290af1c85ac6d53e78e3ab
Antonio <antonio@MacBook-Pro-di-Antonio.local>
2023-12-01 10:30:18 +0100

    Modificati alcuni

--- esercizi/esercizio_4_13.sv
+++ esercizi/esercizio_4_13.sv
@@ -12,7 +12,7 @@ module decoder_2_4 (
          2'b01 : y = 4'b0010;
          2'b10 : y = 4'b0100;
          2'b11 : y = 4'b1000;
-         default: y = 4'b0001;
+         default: y = 4'b0000;
         endcase
  
 endmodule
